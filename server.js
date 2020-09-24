e7a936eedf5d527f649122ca835bb0298a8587c1
Nicole Wiegand <nwiegand05@yahoo.com>
2020-09-24 14:12:34 -0400

    debugging

--- server.js
+++ server.js
@@ -32,5 +32,22 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
 })
 
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+
 // Start Express
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
