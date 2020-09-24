558f7dc66aa2c12c0f0774f609078180a29ccab0
Nicole Wiegand <nwiegand05@yahoo.com>
2020-09-24 14:02:15 -0400

    adding build dir

---
+++ client/build/precache-manifest.3def7f25903e44b924df6d79448be1f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6478ef16c0b8de94fd8f9f06d5133560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1439cc1753c4dbd120c",
+    "url": "/static/css/main.f2f08e83.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1439cc1753c4dbd120c",
+    "url": "/static/js/main.25f16972.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
